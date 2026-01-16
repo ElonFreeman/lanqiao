db45f89de24e65e42ffabbbd3b41c3a2a3dc79e6
lmde <lmde@Legion-laptop>
2026-01-16 10:10:01 +0800

    260116

---
+++ pointer/0001.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;  //标准命名空间
+
+int main()
+{
+    
+    cout<< "hello,fuck you!";
+    cout<< endl;
+    int num=0;
+    char ch='f';
+
+    cout<< ch;
+    cout<< endl;
+    cout<< num;
+
+    return 0;
+}
