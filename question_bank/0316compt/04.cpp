624291707c12d06b8d6033de169502cac3e790c4
ElonFreeman <zhanghao112358@outlook.com>
2026-03-20 17:40:51 +0800

    0320

--- question_bank/0316compt/04.cpp
+++ question_bank/0316compt/04.cpp
@@ -15,13 +15,25 @@ int main(void)
     {
         if(arr.at(i)>0)
         {
-            if()
+            if(i==0)
             {
-
+                cout << arr.at(i) << "x^" << j;
+                continue;
             }
-            if()
+            if(j==0)
             {
-                
+                cout << '+' << arr.at(i);
+                continue;
+            }
+            if(abs(arr.at(i))==1)
+            {
+                cout << '+'<< "x^" << j;
+                continue;
+            }
+            if(abs(arr.at(i))==1&&abs(arr.at(i))==1)
+            {
+                cout << "x^" << j;
+                continue;
             }
             cout << '+' << arr.at(i) << "x^" << j;
         }
@@ -30,6 +42,26 @@ int main(void)
             continue;
         else if(arr.at(i)<0)
         {
+            if(i==0)
+            {
+                cout << arr.at(i) << "x^" << j;
+                continue;
+            }
+            if(j==0)
+            {
+                cout << '+' << arr.at(i);
+                continue;
+            }
+            if(abs(arr.at(i))==1)
+            {
+                cout << '-'<< "x^" << j;
+                continue;
+            }
+            if(abs(arr.at(i))==1&&abs(arr.at(i))==1)
+            {
+                cout <<'-' << "x^" << j;
+                continue;
+            }
             cout << arr.at(i) << "x^" << j;
         }
             
@@ -37,3 +69,66 @@ int main(void)
 
     return 0;
 }
+
+/*#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n = 0;
+    cin >> n;
+    vector<int> arr(n + 1);
+    
+    // 输入系数
+    for(int i = 0; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    bool firstTerm = true;  // 标记是否为第一项
+    
+    for(int i = 0; i <= n; i++)
+    {
+        int coeff = arr[i];
+        int power = n - i;
+        
+        if(coeff == 0) continue;  // 系数为0跳过
+        
+        // 处理符号
+        if(firstTerm)
+        {
+            if(coeff < 0) cout << '-';
+            firstTerm = false;
+        }
+        else
+        {
+            if(coeff > 0) cout << '+';
+            else cout << '-';
+        }
+        
+        // 处理系数（绝对值）
+        int absCoeff = abs(coeff);
+        
+        // 处理常数项
+        if(power == 0)
+        {
+            cout << absCoeff;
+            continue;
+        }
+        
+        // 处理系数为1的情况
+        if(absCoeff != 1)
+        {
+            cout << absCoeff;
+        }
+        
+        // 输出变量部分
+        cout << 'x';
+        if(power > 1)
+        {
+            cout << '^' << power;
+        }
+    }
+    
+    return 0;
+}*/
