0cff7b62901a01ac8dc5cf12edc032b43b5ad802
ElonFreeman <zhanghao112358@outlook.com>
2026-03-22 21:25:45 +0800

    032103

--- question_bank/0099.cpp
+++ question_bank/0099.cpp
@@ -5,16 +5,28 @@ int main(void)
 {
     int n=0,k=0;
     cin >> n >> k;
-    struct n_and_K
+    vector<vector<int>> choco(n,vector<int>(2));  /*0:H,1:W*/
+
+    cout << "input:" <<endl;
+    for(int i=0;i<n;i++)
     {
-        int h;
-        int k;
-    };
-    n_and_K arr[n];
-    for(n_and_K &traverse:arr)
-    {
-        cin >> traverse.h >> traverse.k;
+        cin >> choco.at(i)[0] >> choco.at(i)[1];
     }
+
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=choco.at(i)[0]*choco.at(i)[1];
+    }
+
+    int average=sum/k;
+    int max=average;
+    while(((int)sqrt(max)*10)%10!=0)
+    {
+        max++;
+    }
+
+    cout << (int)sqrt(max) << endl;
     
     return 0;
 }
