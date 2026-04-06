8420ff9490a1b4e2d79519ed906a9b31d1136348
ElonFreeman <zhanghao112358@outlook.com>
2026-04-06 18:47:48 +0800

    040602

--- question_bank/0300_qsort.cpp
+++ question_bank/0300_qsort.cpp
@@ -1,27 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int compare(const void *a,const void *b)
+int compare(const void *a,const void *b)  //比较函数：定义比较规则，静态变量，防止被改变
 {
-    int ia=*((const int*)a),ib=*((const int*)b);
-    if(ia>ib)
-        return 1;
-    else if(ia<ib)
+    int ia=*((const int*)a),ib=*((const int*)b);  //将静态形参转换为实际类型
+    
+    /*比较规则：升序：a<b return -1,降序：a<b return 1*/
+    if(ia<ib)
         return -1;
+    else if(ia>ib)
+        return 1;
     else
         return 0;
 }
 
 int main(void)
 {
-    int n=0;cin >> n;
+    int n;cin >> n;
     vector<int> arr(n);
     for(int &traverse:arr)
     {
         cin >> traverse;
     }
 
-    qsort(&arr,size(arr),sizeof(int),compare);
+    qsort(&arr[0],arr.size(),sizeof(int),compare);  //参数表：首元素地址，元素数量，单个元素大小，比较函数地址
 
     for(int i=0;i<n;i++)
     {
