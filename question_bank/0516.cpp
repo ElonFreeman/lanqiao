60338e4feb1a56b608f14fe31ac4a8c500207231
ElonFreeman <zhanghao112358@outlook.com>
2026-04-07 21:51:11 +0800

    040706

---
+++ question_bank/0516.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int total,plan;cin >> total >> plan;
+    vector<vector<int>> dataset(total,vector<int>(2));
+    for(int i=0;i<total;i++)
+    {
+        cin >> dataset[i][0] >> dataset[i][1];
+    }
+
+    
+
+    return 0;
+}
